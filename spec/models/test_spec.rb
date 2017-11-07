3421ae03c4518d368a5b2125bcb7d3eb0f2c131a
John Marks <jmarks@covermymeds.com>
2017-11-06 20:54:35 -0500

    Initial commit rails rspec postgres

---
+++ spec/models/test_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Test, type: :model do
+
+  context 'something' do
+    context 'lol' do
+      it 'does something lol' do
+        expect(described_class.new).to be_valid
+      end
+    end
+  end
+end
